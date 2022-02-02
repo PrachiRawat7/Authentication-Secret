9e3378f828b101eb499a374978940ed72b70842d
Prachi <prachirawat2002@gmail.com>
2022-02-02 15:17:38 +0530

    Level 3 Hashing Password

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express=require("express");
 const ejs=require("ejs");
 const bodyParser=require("body-parser");
 const mongoose=require("mongoose");
-const encrypt=require("mongoose-encryption");
+const md5=require("md5");
 const app=express();
 
 app.set("view engine","ejs");
@@ -19,7 +19,7 @@ const userSchema=new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
+
 
 
 const User=new mongoose.model("User",userSchema);
@@ -43,7 +43,7 @@ app.post("/register",function(req,res){
 
   const newUser=new User({
      email:req.body.username,
-    password:req.body.password
+    password:md5(req.body.password)
   });
   newUser.save(function(err){
     if(err){
@@ -61,7 +61,7 @@ app.post("/login",function(req,res){
       console.log(err);
     }else{
       if(result){
-        if(result.password===req.body.password){
+        if(result.password===md5(req.body.password)){
         res.render("secrets");}
 
        else{

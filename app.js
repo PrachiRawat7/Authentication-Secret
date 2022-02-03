8f209a3dfc694a8bfaad3834da8564a954beee9d
Prachi <prachirawat2002@gmail.com>
2022-02-03 23:22:09 +0530

    Level 4 Salting and Hashing

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express=require("express");
 const ejs=require("ejs");
 const bodyParser=require("body-parser");
 const mongoose=require("mongoose");
-const md5=require("md5");
+const bcrypt=require("bcrypt");
+const saltRound=10;
 const app=express();
 
 app.set("view engine","ejs");
@@ -41,33 +42,42 @@ app.get("/register",function(req,res){
 
 app.post("/register",function(req,res){
 
-  const newUser=new User({
-     email:req.body.username,
-    password:md5(req.body.password)
+  bcrypt.hash(req.body.password,saltRound,function(err,hash){
+    const newUser=new User({
+       email:req.body.username,
+      password:hash
+    });
+    newUser.save(function(err){
+      if(err){
+        console.log(err);
+      }else {
+        res.render("secrets");
+      }
+    });
   });
-  newUser.save(function(err){
-    if(err){
-      console.log(err);
-    }else {
-      res.render("secrets");
-    }
-  });
+
+
 });
 
 
 app.post("/login",function(req,res){
-  User.findOne({email:req.body.username},function(err,result){
+  User.findOne({email:req.body.username},function(err,foundUser){
     if(err){
       console.log(err);
     }else{
-      if(result){
-        if(result.password===md5(req.body.password)){
-        res.render("secrets");}
+      if(foundUser){
+        bcrypt.compare(req.body.password,foundUser.password,function(err,result){
+          if(result===true){
+            res.render("secrets");
+          }else{
+            console.log("password doesn't match");
+          }
 
-       else{
-        console.log("Not Found");
+        });
       }
-    }
+      else{
+       console.log("Not Found");
+     }
   }
   });
 });
